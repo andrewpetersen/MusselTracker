8048e29a953da71cef2c749cef70893c45e6fe14
Henry Jordan <hjordan5@ab201.cs.lmu.edu>
2014-06-05 18:13:57 -0700

    got sleep mode working

--- Arduino/MusselTracker.ino
+++ Arduino/MusselTracker.ino
@@ -1,5 +1,3 @@
-
-
 /* 
 Our Master Arduino code file
 
@@ -37,6 +35,7 @@ int month;
 const int pVcc = A0;  //Pin for Vcc
 const int pVo = A1;   //Pin for Vo
 const int hallPin = A2;
+const int rtcInterrupt = 2;
 const int heartbeatLED = 8;
 const int errorLED = 9;
 const int chipSelect = 10;
@@ -47,29 +46,18 @@ const int delayLength = 450; // delay in millisec
 
 
 // Initialize the RTC
-void startTime(){
+void startTime() {
+
+  // If you want to set the time and date, this is here to do it
   
-  // This block sets an alarm every second
-  Wire.beginTransmission(0x68);                              
-  Wire.write(byte(0x07));           
-  Wire.write(byte(0x80));
-  Wire.write(byte(0x80));
-  Wire.write(byte(0x80));
-  Wire.write(byte(0x80));
-  Wire.endTransmission(); 
   
   // This block puts 0x00 in the control register, starting the RTC
+  // It also configures the RTC to output a 1Hz SQW
   // The date should be set before this code runs
   Wire.beginTransmission(0x68);                              
   Wire.write(byte(0x0E));           
   Wire.write(byte(0x00));
   Wire.endTransmission();     
-  
-  
-  // Reset the pointer to 0x00 for easy reading
-  Wire.beginTransmission(0x68);                         
-  Wire.write(byte(0x00));          
-  Wire.endTransmission();
   
 }
 
@@ -118,12 +106,96 @@ void displayError() {
   
 }  
 
+void collectData() {
+ 
+   // heartbeat LED flash
+  digitalWrite(heartbeatLED, HIGH); 
+  
+  // Read in analog pins (Hall Effect and Thermistor)
+  hall = analogRead(hallPin);
+  Vcc = analogRead(pVcc);            
+  Vo = analogRead(pVo);            
+  
+  //  Read in IMU data (comment for testing without cables)
+  //lsm.read(); 
+  
+  // Read in current time       
+  getTime();       
+  
+  // Check bounds and set errorLED
+  displayError();
+
+
+  // Build final output string
+  sprintf(outputString, "%s\t%5d\t%5d\t%5d\t%5d\t%5d\t%5d\t%5d\t%5d\t%5d", 
+     timeString,
+     (int)lsm.accelData.x, (int)lsm.accelData.y, (int)lsm.accelData.z, 
+    (int)lsm.magData.x, (int)lsm.magData.y, (int)lsm.magData.z, Vcc, Vo,  hall);
+
+  // Print the output to Serial 
+  Serial.println(outputString);
+
+  // Set up file name and print data to file
+  sprintf(fileName,"MT%2.2d%2.2d%2.2d.txt", month, day, hour);
+  
+  File dataFile = SD.open(fileName, FILE_WRITE);
+  
+  if (dataFile) {
+    dataFile.println(outputString);
+    dataFile.close();
+  }  
+  // if the file isn't open, pop up an error:
+  else {
+    digitalWrite(errorLED, HIGH);
+    Serial.println("error opening datalog.txt");
+  }
+  
+  
+  // heartbeat/delay (needs to switch to sleep)           
+  digitalWrite(heartbeatLED, LOW);
+}
+
+// Wake up from sleep here
+void pin2Interrupt(void)
+{
+  /* This will bring us back from sleep. */
+  
+  /* We detach the interrupt to stop it from 
+   * continuously firing while the interrupt pin
+   * is low.
+   */
+  detachInterrupt(0);
+}
+
+
+// Use this to go to sleep
+void enterSleep(void)
+{
+  
+  /* Setup pin2 as an interrupt and attach handler. */
+  attachInterrupt(0, pin2Interrupt, LOW);
+  delay(10);
+  
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  
+  sleep_enable();
+  
+  sleep_mode();
+  
+  /* The program will continue from here. */
+  
+  /* First thing to do is disable sleep. */
+  sleep_disable(); 
+}
+
+
 // the setup routine runs once when you press reset
 void setup() {
   
     // Heartbeat LED
   pinMode(heartbeatLED, OUTPUT);
   pinMode(errorLED, OUTPUT);
+  pinMode(rtcInterrupt, INPUT);
   
   digitalWrite(heartbeatLED, HIGH);
   digitalWrite(errorLED, HIGH);
@@ -161,51 +233,21 @@ void setup() {
 // the loop routine runs over and over again forever:
 void loop() {
   
-  // heartbeat LED flash
-  digitalWrite(heartbeatLED, HIGH); 
+  collectData();
   
-  // Read in analog pins (Hall Effect and Thermistor)
-  hall = analogRead(hallPin);
-  Vcc = analogRead(pVcc);            
-  Vo = analogRead(pVo);            
-  
-  //  Read in IMU data (comment for testing without cables)
-  //lsm.read(); 
-  
-  // Read in current time       
-  getTime();       
-  
-  // Check bounds and set errorLED
-  displayError();
-
-
-  // Build final output string
-  sprintf(outputString, "%s\t%5d\t%5d\t%5d\t%5d\t%5d\t%5d\t%s\t%5d\t%5", 
-     timeString,
-     (int)lsm.accelData.x, (int)lsm.accelData.y, (int)lsm.accelData.z, 
-    (int)lsm.magData.x, (int)lsm.magData.y, (int)lsm.magData.z, Vcc, Vo,  hall);
-
-  // Print the output to Serial 
-  Serial.println(outputString);
-
-  // Set up file name and print data to file
-  sprintf(fileName,"MT%2.2d%2.2d%2.2d.txt", month, day, hour);
-  
-  File dataFile = SD.open(fileName, FILE_WRITE);
-  
-  if (dataFile) {
-    dataFile.println(outputString);
-    dataFile.close();
-  }  
-  // if the file isn't open, pop up an error:
-  else {
-    digitalWrite(errorLED, HIGH);
-    Serial.println("error opening datalog.txt");
+  while( !digitalRead(2)) {
+      delay(1);
   }
   
+  collectData();
   
-  // heartbeat/delay (needs to switch to sleep)           
-  digitalWrite(heartbeatLED, LOW);
-  delay(delayLength);
+  enterSleep();
+  
+  // Use this if you don't want to sleep
+  //while( digitalRead(2)) {
+  //    delay(1);
+  //}
+
   
 }
+
