95886ec35a542bc1095797c4c5a942567c9988cf
Andrew Petersen <andrew.petersen@uconn.edu>
2014-06-18 18:12:30 -0700

    MATLAB code runs again with data. Fixed axis, works with timestamps and resets.

--- MATLAB/MusselsDataAnalysis.m
+++ MATLAB/MusselsDataAnalysis.m
@@ -107,7 +107,25 @@ end
 fclose(fid);
 fprintf('Finished reading data from %s \n',dataToProcessFileName);
 
-x_axis = 1:length(musselData);
+%Get Rk and Rt@25 from SDXX.TXT file
+fid = fopen(strcat(workingDirectory,fileSDName));
+if fid == -1
+    error('Could not open %s',fileSDName)
+end
+fgetl(fid); %skip first line
+RtFromFile = fgetl(fid);
+RkFromFile = fgetl(fid);
+fclose(fid);
+if ~((length(RtFromFile)>2 && strcmp(RtFromFile(1:2),'Rt')) && length(RkFromFile)>2 && strcmp(RkFromFile(1:2),'Rk'))
+    warning('SDXX file in wrong format.')
+    fprintf('Proper format is: \nSDXX\nRt xxxxx\nRk xxxxx\n')
+    error('SDXX file in wrong format')
+end    
+Rt = str2num(char(RtFromFile(3:end)));
+Rk = str2num(char(RkFromFile(3:end)));
+
+fprintf('Rt@25 = %d   (at 25 degrees Celsius)\n',Rt);
+fprintf('Rk    = %d   (known resistance)\n',Rk);
 
 imuMatrix = musselData(:,1:6);
 thermistorResistanceData = musselData(:,7:8);
@@ -115,10 +133,9 @@ hallEffectData = musselData(:,9);
 
 %Calculating Thermistor Resistance
 disp('Thermistor calculations started')
-Rk = 9800;
 I = (thermistorResistanceData(:,1)-thermistorResistanceData(:,2))./(Rk);
 thermistorResistances = thermistorResistanceData(:,2)./I;
-thermistorTemp = resistanceToTemp(thermistorResistances, 10000);
+thermistorTemp = resistanceToTemp(thermistorResistances, Rt);
 disp('Thermistor calculations finished')
 
 %Calculating Distance from Hall Effect Sensor
@@ -126,8 +143,27 @@ disp('Hall effect calculations started')
 hallEffectDistance = hallVoltsToDist(hallEffectData);
 disp('Hall effect calculations finished')
 
+%Convert Timestamps to format for datenum and datetick
+timeTicks = zeros(length(timestamps(:,1)),21);
+timeTicks(:,1:17) = timestamps;
+for i=1:length(timeTicks(:,1))
+    timeTicks(i,18:21) = '.000';
+end
+
+a = timeTicks(1,:);
+for i=2:length(timeTicks(:,1))
+    b = timeTicks(i,:);
+    if strcmp(char(a),char(b))
+        timeTicks(i,19)='5';
+    end
+    a = b;
+end
+% x_axis = datenum(char(timeTicks),'yy/mm/dd HH:MM:SS.FFF');
+x_axis = datenum(datevec(char(timeTicks)));
+
 disp('Plotting')
-set(figure, 'color', [1 1 1])
+fHandle = figure('units','normalized','outerposition',[0.1 0.1 0.9 0.9]);%makes figure fullscreen (not 'Maximized')
+set(fHandle, 'color', [1 1 1])
 subplot(3,1,1)
 hold on
 plot(x_axis, thermistorTemp,'o','MarkerSize',1)
@@ -136,19 +172,55 @@ axis([-inf, inf, 5, 45])
 
 subplot(3,1,2)
 hold on;
-plot(x_axis, hallEffectDistance,'r')
+plot(x_axis, hallEffectDistance,'ro','MarkerSize',1)
 title('Hall Effect','FontWeight','bold','FontSize',15);ylabel('Gape, mm');
-axis([-inf, inf, -1,3])
+axis([-inf, inf, -1,6])
 
 subplot(3,1,3)
 hold on
-title('IMU Data: Red-Acc, Blue-Mag','FontWeight','bold','FontSize',15);xlabel('Data Points');
-axis([-inf, inf, -2000, 2000])
+title('IMU Data: Red-Acc, Blue-Mag','FontWeight','bold','FontSize',15);xlabel(['Timestamps, Date: ',dateCollected(1:5)]);
+axis([-inf, inf, -5000, 5000])
 for i=1:3
-    plot(x_axis,imuMatrix(:,i),'r')
+    plot(x_axis,imuMatrix(:,i),'ro','MarkerSize',1)
 end
 for i=4:6
-    plot(x_axis,imuMatrix(:,i),'b')
+    plot(x_axis,imuMatrix(:,i),'bo','MarkerSize',1)
 end
 
+%Getting Timestamps of Resets
+fid = fopen([workingDirectory,fileErrorLogName]);
+if fid == -1
+    warning('Could not open %s',fileErrorLogName)
+end
+fgetl(fid); %ignore start time 
+resets = [];
+while ~feof(fid)
+    resets = [resets; fgetl(fid)];
+end
+fclose(fid);
 
+%'Reset pressed at 14/06/17 13:40:54'
+for i=1:length(resets(:,1))
+    time = [resets(i,18:end),'.000'];%this code could be better, resets might be off by 0.5 seconds
+    xtime = datenum(datevec(char(time)));
+    x = [xtime, xtime];
+    y = [-20000,20000];
+    subplot(3,1,1)
+    plot(x,y,'g-')
+    subplot(3,1,2)
+    plot(x,y,'g-')
+    subplot(3,1,3)
+    plot(x,y,'g-')
+end
+
+%DateTickZoom
+subplot(3,1,1)
+datetickzoom('x','HH:MM:SS.FFF')
+subplot(3,1,2)
+datetickzoom('x','HH:MM:SS.FFF')
+subplot(3,1,3)
+datetickzoom('x','HH:MM:SS.FFF')
+
+
+%Save the plot alongside the concatenated data file
+savefig(fHandle,[subFolder,dateSDNum])
